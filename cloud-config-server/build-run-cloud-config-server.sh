07775e4048e9149d99b7991fab51bc849548e8aa
Jerry Santiago <jerrysantiago128@gmail.com>
2025-05-08 12:19:43 -0400

    adding files to build and run the server with Docker

---
+++ cloud-config-server/build-run-cloud-config-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build the source code 
+mvn clean package
+
+
+# build the docker image
+docker build -t cloud-config-server:0.0.1 .
+
+# start the app with 'docker run'
+
+docker run -p 8888:8888 cloud-config-server:0.0.1
+
+# OR start the app with 'docker compose'
+
+#docker compose -f cloud-config-server.yml up
