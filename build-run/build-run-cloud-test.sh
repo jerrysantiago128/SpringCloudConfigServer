93e17728323215f6ebc6cd0a79db0ca8e3c1cd26
Jerry Santiago <jerrysantiago128@gmail.com>
2025-05-13 07:50:01 -0400

    migrating build-run scripts to 'build-run' directory

---
+++ build-run/build-run-cloud-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# remove application.properties file and move '.env' file to current directory
+cp ../config/cloud-config.properties test-app/src/main/resources/application.properties
+
+cd ../test-app/
+
+# build the source code 
+mvn clean package
+
+cd ../
+# build the docker image
+docker build -t spring-app:cloud-config .
+
+# start the app with 'docker run'
+
+#docker run -p 8080:8080  --name spring-app-cloud spring-app:cloud-config
+
+# OR start the app with 'docker compose'
+cd compose/
+
+docker compose -f spring-app-cloud.yml up
